b27f046ae3b715949dce4e5f9c3225cb1826e38d
Jaroslav Havrda <44003662+Jara69@users.noreply.github.com>
2020-12-07 18:34:29 +0100

    Add files via upload

---
+++ EduWebik/webik/User.java
@@ -0,0 +1,58 @@
+package cz.educanet.webik;
+
+public class User {
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private String email;
+
+    public User(String email ,String username, String password, String firstname, String lastname) {
+        this.username = username;
+        this.password = password
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFirstname(){
+        this.firstname = firstname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+
+    public void setLastname(){
+        this.lastname = lastname;
+    }
+
+    public String getLastname(){
+        return lastname;
+    }
+
+    public void setEmail(){
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
---
+++ EduWebik/webik/UserManager.java
@@ -0,0 +1,61 @@
+package cz.educanet.webik;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserManager {
+
+    private static List<User> names = new ArrayList<User>();
+
+    @POST
+    public Response createUser(@QueryParam("Email") String email, @QueryParam("First name") String firstname, @QueryParam("Last name") String lastname ,@QueryParam("username") String username, @QueryParam("password") String password) {
+        User tempUser = new User(firstname, lastname ,username,password);
+        if(doesUserExist(tempUser)){
+            return Response.status(406).build();
+        } else {
+            names.add(tempUser);
+            return Response.ok("User Created").build();
+        }
+    }
+
+    public Boolean doesUserExist(User user) {
+        for (int i = 0; i < names.size(); i++) {
+            if (names.get(i).getUsername().equals(user.getUsername())) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    @DELETE
+    public Response removeUser(User user) {
+        if(doesUserExist(user)) {
+            names.remove(user);
+            return Response.ok().build();
+        } else {
+            return Response.status(406).build();
+        }
+    }
+    @GET
+        public Response GetCurrentUser(User user){
+        return user;
+
+
+    }
+
+    @POST
+            public Response LoginUser(@QueryParam("username") String username, @QueryParam("password") String password){
+        if (doesUserExist(user)){
+
+
+        }
+
+    }
+}
