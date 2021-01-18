5122aef73c068cd280b65c1fe8ab440812375ea2
Jaroslav Havrda <Jaroslav.Havrda@educanet.cz>
2021-01-18 20:49:56 +0100

    hmu

---
+++ src/main/java/cz/educanet/webik/caretakerManager.java
@@ -0,0 +1,15 @@
+package cz.educanet.webik;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+
+public class caretakerManager {
+
+    private static List<caretaker> names = new ArrayList<caretaker>();
+
+}
---
+++ src/main/java/cz/educanet/webik/caretakerResource.java
@@ -0,0 +1,9 @@
+package cz.educanet.webik;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+public class caretakerResource {
+
+}
